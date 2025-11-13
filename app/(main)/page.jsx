51595b8fc35c027dc9dfcf59f213b3ed548e76c3
Mohammadreza Abdollahi <mohammad1384abdollahi@gmail.com>
2025-11-13 20:54:54 +0330

    version 0.5.2
    Fix For Deploy

--- app/(main)/page.jsx
+++ app/(main)/page.jsx
@@ -3,16 +3,23 @@ import Articles from "@/components/articles/Articles";
 import Email from "@/components/email/Email";
 import Products from "@/components/products/Products";
 import LandingHeadSlider from "@/components/sliders/LandingHeadSlider";
+export const dynamic = "force-dynamic";
 const getSlides = async () => {
-  const res = await fetch(`${process.env.SITE_URL}/api/slides`,{next: {revalidate: 86400}});
+  const res = await fetch(`${process.env.SITE_URL}/api/slides`, {
+    next: { revalidate: 86400 },
+  });
   return res.json();
 };
 const getArticles = async () => {
-  const res = await fetch(`${process.env.SITE_URL}/api/articles`,{next: {revalidate: 0}});
+  const res = await fetch(`${process.env.SITE_URL}/api/articles`, {
+    next: { revalidate: 0 },
+  });
   return res.json();
 };
 const getProducts = async () => {
-  const res = await fetch(`${process.env.SITE_URL}/api/products/latest`,{next: {revalidate: 0}});
+  const res = await fetch(`${process.env.SITE_URL}/api/products/latest`, {
+    next: { revalidate: 0 },
+  });
   return res.json();
 };
 export default async function Home() {
@@ -21,11 +28,11 @@ export default async function Home() {
   const products = await getProducts();
   return (
     <div>
-      <LandingHeadSlider slides={slides}/>
-      <Activities/>
-      <Products products={products}/>
-      <Email/>
-      <Articles articles={articles}/>
+      <LandingHeadSlider slides={slides} />
+      <Activities />
+      <Products products={products} />
+      <Email />
+      <Articles articles={articles} />
     </div>
   );
 }
